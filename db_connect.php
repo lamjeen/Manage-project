e126e8db42d2570feb7a691a98b8697c725532db
Lam Jeen Sin Anthony <55839741+lamjeen@users.noreply.github.com>
2025-12-14 23:57:52 +0700

    7.0

--- db_connect.php
+++ db_connect.php
@@ -2,15 +2,11 @@
  $host = 'localhost';
  $dbname = 'project_management';
  $username = 'root';
- $password = '';
+ $password = 'root';
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch(PDOException $e) {
-    die("ERROR: Could not connect. " . $e->getMessage());
-}
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 ?>
